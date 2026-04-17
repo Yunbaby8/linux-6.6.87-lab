52e8c3318c5a3fdb0f20bd808bdfeb00fa5c6c15
Eric ZHou <2832902251@qq.com>
2026-04-17 10:49:05 +0800

    clean tracked build artifacts and update gitignore

--- include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"x86_64"
-#define LINUX_COMPILE_BY	"yunhe"
-#define LINUX_COMPILE_HOST	"yunhe-virtual-machine"
-#define LINUX_COMPILER		"gcc (Ubuntu 11.4.0-1ubuntu1~22.04.3) 11.4.0, GNU ld (GNU Binutils for Ubuntu) 2.38"
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "6.6.87-yunhe-v1+"
